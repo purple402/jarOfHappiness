29e064552e822c598a84c467da5ca3f843d280ae
Joy Lee <purple402@naver.com>
2022-03-28 21:16:16 +0900

    Create Main page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="titleDiv">
+        <div className="title">
+          <span id="emoji">✏️</span>
+          <span id="appTitle">해피 저금통</span>
+          <p id="titleInfo">
+            기쁜 일이나 즐거운 일을 적어 넣어주세요. 한 해 동안 모아둔 행복한
+            순간을 연말에 개봉해 꺼내보며 한 해의 기쁨을 떠올려봐요.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
