55c8d2868f079134d41dcf31fbc797ac820e7597
purple402 <purple402@naver.com>
2022-03-28 22:14:22 +0900

    Move titleInfo

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
         <div className="title">
           <span id="emoji">✏️</span>
           <span id="appTitle">해피 저금통</span>
-          <p id="titleInfo">
-            기쁜 일이나 즐거운 일을 적어 넣어주세요. 한 해 동안 모아둔 행복한
-            순간을 연말에 개봉해 꺼내보며 한 해의 기쁨을 떠올려봐요.
-          </p>
         </div>
+        <p id="titleInfo">
+          기쁜 일이나 즐거운 일을 적어 넣어주세요. 한 해 동안 모아둔 행복한
+          순간을 연말에 개봉해 꺼내보며 한 해의 기쁨을 떠올려봐요.
+        </p>
       </div>
       {user === null ? (
         <Login onSubmit={(user) => handleSubmit(user)} />
