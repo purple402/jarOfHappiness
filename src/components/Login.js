7bc871e41af5b61f27baaf977e3bbf0e324b4c8b
purple402 <purple402@naver.com>
2022-03-28 22:16:31 +0900

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React from "react";
 import { login } from "../firebase.js";
+import "./Login.css";
 
 function Login(props) {
     async function handleSubmit(e) {
@@ -8,20 +9,32 @@ function Login(props) {
         const password = e.target[1].value;
         const user = await login(id, password);
         props.onSubmit(user);
+    }
 
-      }
-    
     return (
         <div className="Login">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="id">ID</label>
-          <input type="email" id="id" name="id" required />
-          <label htmlFor="password">PASSWORD</label>
-          <input type="password" id="password" name="password" required />
-          <input type="submit" value="login" />
-        </form>
-      </div>
-    )
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="id">ID</label>
+                    <input type="email" id="id" name="id" required />
+                </div>
+                <div>
+                    <label htmlFor="password">PASSWORD</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                    />
+                </div>
+                <input type="submit" value="로그인" />
+            </form>
+            <div className="signup">
+                <span>회원이 아니신가요?</span>
+                <input type="button" value="회원가입" />
+            </div>
+        </div>
+    );
 }
 
 export default Login;
