23b25ddaa318121176d7083a7b2fd66bd6c3208c
Joy Lee <purple402@naver.com>
2022-04-06 16:27:47 +0900

    Create UpdateProfile component

---
+++ src/components/UpdateProfile.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { getCurrentUser, updateUserDisplayName } from "../firebase";
+
+function UpdateProfile(props) {
+  const user = getCurrentUser();
+  const [displayName, setDisplayName] = useState(user.displayName);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if(displayName !== user.displayName) {
+      await updateUserDisplayName(displayName);
+    }
+    props.finishUpdateProfile();
+  }
+
+  function handleDisplayName(e) {
+    setDisplayName(e.target.value);
+  }
+
+  function closeModal() {
+    props.finishUpdateProfile()
+  }
+
+  return (
+    <div className="UpdateProfile">
+      <div>
+        <span>프로필 수정</span>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="id">이메일</label>
+          <input
+            type="email"
+            id="id"
+            name="id"
+            value={user.email}
+            readOnly
+            disabled
+          />
+        </div>
+        <div>
+          <label htmlFor="displayName">별명</label>
+          <input
+            id="displayName"
+            name="displayName"
+            value={displayName || undefined}
+            onChange={handleDisplayName}
+            placeholder={displayName === null ? "별명이 없습니다" : ""}
+          />
+        </div>
+        <input type="submit" value="적용" />
+        <input type="button" value="취소" onClick={() => closeModal()}/>
+      </form>
+    </div>
+  );
+}
+
+export default UpdateProfile;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Login from "./Login.js";
 import Modal from "./Modal.js";
 import Signup from "./Signup.js";
+import UpdateProfile from "./UpdateProfile.js";
 
-export { Login, Modal, Signup };
+export { Login, Modal, Signup, UpdateProfile };
