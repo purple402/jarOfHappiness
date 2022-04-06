520eedba12a05de85dc450dffd24046610514074
Joy Lee <purple402@naver.com>
2022-04-06 17:34:00 +0900

    Create Writing screen

---
+++ src/screens/Writing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Writing() {
+    return (
+        <div className="Writing">
+            <div className="title">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Writing
