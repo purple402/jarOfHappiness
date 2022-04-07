3091659773d4891f0cbad168465c5ef95c169898
Joy Lee <purple402@naver.com>
2022-04-07 22:23:21 +0900

    Update Writing component

--- src/screens/Writing.js
+++ src/screens/Writing.js
@@ -1,13 +1,32 @@
-import React from 'react'
-
 function Writing() {
-    return (
-        <div className="Writing">
-            <div className="title">
-                
-            </div>
+import React from "react";
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("handleSubmit");
+  }
+
+  function handleCancel() {
+  }
+
+  return (
+    <div className="Writing">
+      <div className="titleDiv">
+        <div className="title">
+          <span id="emoji">✏️</span>
+          <span id="appTitle">해피 저금통</span>
         </div>
-    )
+      </div>
+      <div className="writingDiv">
+        <form onSubmit={handleSubmit}>
+          <textarea placeholder="나중에 다시 보고싶은 행복을 적어주세요" />
+          <input id="date" type="date"/>
+          <input type="submit" value="저금통에 넣기" />
+          <input type="button" value="다음에 적기" onClick={handleCancel} />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Writing
