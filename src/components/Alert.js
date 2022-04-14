5c8f1529531822b5f60fa35629078e3670099e8a
Joy Lee <purple402@naver.com>
2022-04-14 22:22:16 +0900

    Use messageRef instead of message variable

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,19 +1,24 @@
-import React, {useRef} from "react";
-import './Alert.css'
+import React, { useRef, useEffect, useState } from "react";
+import "./Alert.css";
 
 function Alert(props) {
-const messageRef = useRef(null);
-
+  const [show, setShow] = useState(true);
   const code = props.code;
-  let message = null;
-  switch(code) {
+  const messageRef = useRef();
+
+  useEffect(() => {
+    switch (code) {
       case "auth/user-not-found":
-          message = `아이디가 존재하지 않습니다. 아이디를 확인해주세요.`
-          break;
-    case "auth/wrong-password":
-        messageRef.current.innerHTML = '비밀번호가 틀렸습니다. </br> 비밀번호를 확인해주세요.'
+        messageRef.current.innerHTML = `아이디가 존재하지 않습니다. </br> 아이디를 확인해주세요.`;
         break;
-  }
+      case "auth/wrong-password":
+        messageRef.current.innerHTML =
+          "비밀번호가 틀렸습니다. </br> 비밀번호를 확인해주세요.";
+        break;
+      default:
+        messageRef.current.innerHTML =
+          "오류가 발생했습니다. </br> 잠시 후 다시 시도해주세요.";
+    }
   return (
     <div className="Alert">
       <div className="alertBody">
