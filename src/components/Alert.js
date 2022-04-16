7f565673462ec82c38a9e55fe655e34353f2dab5
Joy Lee <purple402@naver.com>
2022-04-16 23:15:17 +0900

    Change show state

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,11 +2,12 @@ import React, { useRef, useEffect, useState } from "react";
 import "./Alert.css";
 
 function Alert(props) {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(false);
   const code = props.code;
   const messageRef = useRef();
 
   useEffect(() => {
+    setShow(true);
     switch (code) {
       case "auth/user-not-found":
         messageRef.current.innerHTML = `아이디가 존재하지 않습니다. </br> 아이디를 확인해주세요.`;
@@ -27,7 +28,7 @@ function Alert(props) {
 
   let timer2 = setTimeout(() => {
     props.finishAlert(null);
-  }, 3000);
+  }, 2500);
 
   return (
     <div className="Alert">
