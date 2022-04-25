9b49cb035238c29eb336909fb6d9d6e66fd9419b
Joy Lee <purple402@naver.com>
2022-04-25 17:13:56 +0900

    update EmojiPicker to detect clicking outside of component

--- src/components/EmojiPicker.js
+++ src/components/EmojiPicker.js
@@ -2,6 +2,29 @@ import React, { useEffect, useRef } from "react";
 import Picker from "emoji-picker-react";
 
 function EmojiPicker(props) {
+  const pickerRef = useOutsideRef(null);
+
+  function useOutsideRef() {
+    const ref = useRef(null);
+
+    useEffect(() => {
+      // picker 외부 클릭하면 picker 사라짐
+      function handleClickOutside(event) {
+        // emojiPicker가 나와있을 때만 작동
+        if (props.displayEmojiPicker) {
+          if (ref.current && !ref.current.contains(event.target)) {
+            props.setDisplayEmojiPicker(!props.displayEmojiPicker);
+          }
+        }
+      }
+      document.addEventListener("click", handleClickOutside);
+      return () => {
+        document.removeEventListener("click", handleClickOutside);
+      };
+    });
+    return ref;
+  }
+
   const onEmojiClick = (event, emojiObject) => {
     props.selectEmoji(emojiObject.emoji);
   };
@@ -13,6 +36,7 @@ function EmojiPicker(props) {
           ? "emojiPickerContainer display"
           : "emojiPickerContainer"
       }
+      ref={pickerRef}
     >
       <Picker onEmojiClick={onEmojiClick} />
     </div>
