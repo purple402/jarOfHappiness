f5840572cecdc8e91cd4f96383e1fcc17c256bc0
Joy Lee <purple402@naver.com>
2022-04-25 17:14:16 +0900

    Update index files with EmojiPicker

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Signup from "./Signup.js";
 import UpdateProfile from "./UpdateProfile.js";
 import UserData from "./UserData.js";
 import Alert from "./Alert.js";
+import EmojiPicker from "./EmojiPicker.js";
 
-export { Login, Modal, Signup, UpdateProfile, UserData, Alert };
+export { Login, Modal, Signup, UpdateProfile, UserData, Alert, EmojiPicker };
