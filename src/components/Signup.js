be713a6d8a5d46acf6385e5db46b9fd51f5648f5
Joy Lee <purple402@naver.com>
2022-05-04 17:01:50 +0900

    Update Signup.js using inputs state

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createUser } from "../firebase";
+import { Message } from "../components";
 
 function Signup(props) {
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    displayName: "",
+  });
+  const { email, password, passwordConfirm, displayName } = inputs;
+
+  function onChange(e) {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  useEffect(() => {
+    checkPassword();
+  }, [password, passwordConfirm]);
+
   async function handleSubmit(e) {
     e.preventDefault();
-    const email = e.target[0].value;
-    const password = e.target[1].value;
-    const displayName = e.target[2].value;
     const user = await createUser(email, password, displayName);
-    props.finishSignup(user);
+    if (typeof user !== "string") {
+      props.finishSignup(user);
+    } else {
+    }
+  }
+
+  function checkPassword() {
+    // 비밀번호와 비밀번호 확인 비교
+    if (password) {
+      if (password.length < 6) {
+        setPasswordMessage(
+          <Message color="gray">
+            비밀번호는 여섯 글자 이상 입력해 주세요.
+          </Message>
+        );
+      } else if (password !== passwordConfirm) {
+        setPasswordMessage(
+          <Message color="orange">비밀번호와 같지 않습니다</Message>
+        );
+      } else {
+        setPasswordMessage(
+          <Message color="green">비밀번호와 동일합니다.</Message>
+        );
+      }
+    } else {
+      // 비밀번호확인만 입력된 경우
+      if (passwordConfirm) {
+        setPasswordMessage(
+          <Message color="gray">비밀번호를 입력해주세요</Message>
+        );
+      }
+    }
   }
 
   return (
@@ -19,15 +69,49 @@ function Signup(props) {
       <form onSubmit={handleSubmit}>
         <div className="modalFormDiv">
           <label htmlFor="id">이메일</label>
-          <input type="email" id="id" name="id" required />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="modalFormDiv">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" id="password" name="password" required />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="modalFormDiv">
+          <label htmlFor="password">비밀번호확인</label>
+          <input
+            type="password"
+            id="passwordConfirm"
+            name="passwordConfirm"
+            minLength="6"
+            value={passwordConfirm}
+            onChange={onChange}
+            required
+          />
+        </div>
+        {passwordMessage}
+        <div className="modalFormDiv">
           <label htmlFor="displayName">별명</label>
-          <input id="displayName" name="displayName" required />
+          <input
+            id="displayName"
+            name="displayName"
+            value={displayName}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="modalBtnDiv">
           <input
@@ -36,7 +120,12 @@ function Signup(props) {
             className="yellowBtn"
             onClick={() => props.finishSignup(null)}
           />
-          <input type="submit" className="greenBtn" value="회원가입" />
+          <input
+            type="submit"
+            className="greenBtn"
+            value="회원가입"
+            disabled={false}
+          />
         </div>
       </form>
     </div>
