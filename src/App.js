808f41e995fe7596ca6a59df45a026c843b6e304
Joy Lee <purple402@naver.com>
2022-08-26 20:55:19 +0900

    Use Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Login } from "./screens";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login, Main, Write } from "./screens";
 import { UserProvider } from "./UserContext";
 import { WindowWidthProvider } from "./WindowWidthContext";
 import "./App.css";
 
 function App() {
+
   return (
-    <WindowWidthProvider>
-      <UserProvider>
-        <div className="App">
-          <Login />
-        </div>
-      </UserProvider>
-    </WindowWidthProvider>
+    <BrowserRouter>
+      <WindowWidthProvider>
+        <UserProvider>
+          <div className="App">
+            <Routes>
+              <Route path="/jarOfHappiness" element={<Login />} />
+              <Route path="/jarOfHappiness/main" element={<Main />} />
+              <Route path="/jarOfHappiness/write" element={<Write />} />
+              <Route
+                path="*"
+                element={
+                  <>
+                    <p>주소를 다시 확인해주세요!</p>
+                  </>
+                }
+              />
+            </Routes>
+          </div>
+        </UserProvider>
+      </WindowWidthProvider>
+    </BrowserRouter>
   );
 }
 
