e870fc75824065c06e277bbaa8202bfc9b6654ce
Joy Lee <purple402@naver.com>
2022-08-26 17:52:01 +0900

    Use contexts in App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
-import React, { useState, useEffect } from "react";
 import { Main, Writing } from "./screens";
+import React from "react";
+import { UserProvider } from "./UserContext";
+import { WindowWidthProvider } from "./WindowWidthContext";
 import "./App.css";
 
-// 화면 크기 구함
-function getWindowDimensions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return { width, height };
-}
-
 function App() {
-  const [user, setUser] = useState(null);
-  const [isWriting, setIsWriting] = useState(false);
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
-
-  useEffect(() => {
-    // 화면 크기 변화를 감지함
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
-    <div className="App">
-      {!isWriting ? (
-        <Main
-          user={user}
-          onChangeUser={(user) => setUser(user)}
-          startWriting={() => setIsWriting(true)}
-          windowWidth={windowDimensions.width}
-        />
-      ) : (
-        <Writing
-          finishWriting={() => setIsWriting(false)}
-          windowWidth={windowDimensions.width}
-        />
-      )}
-    </div>
+    <WindowWidthProvider>
+      <UserProvider>
+        <div className="App">
+        </div>
+      </UserProvider>
+    </WindowWidthProvider>
   );
 }
 
