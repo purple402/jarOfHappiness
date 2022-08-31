813e15e5a10d8b14a9c03c8048e58c9be4d03cf4
Joy Lee <purple402@naver.com>
2022-08-31 20:52:06 +0900

    Use styled-components in App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,45 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import { UserProvider } from "./UserContext";
 import { WindowWidthProvider } from "./WindowWidthContext";
-import "./App.css";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'Cafe24SsurroundAir';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24SsurroundAir.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+body {
+  background-color: #f4f8ff;
+}
+
+body, button, input, textarea {
+  font-family: "Cafe24SsurroundAir";
+}
+
+input[type="submit"]:hover,
+input[type="button"]:hover,
+button {
+  cursor: pointer;
+}
+`;
+
+const RootDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
-
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <WindowWidthProvider>
         <UserProvider>
-          <div className="App">
+          <RootDiv>
             <AppRoutes />
-          </div>
+          </RootDiv>
         </UserProvider>
       </WindowWidthProvider>
     </BrowserRouter>
