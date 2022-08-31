ec2063d4fca7dd22451d2e03a4a06ef3828e27aa
Joy Lee <purple402@naver.com>
2022-08-31 20:52:18 +0900

    Use styled-components in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,70 @@
 import React from "react";
 import { useWindowWidth } from "../WindowWidthContext";
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (max-width: 730px) {
+    margin-top: 30px;
+  }
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 30px;
+
+  @media (max-width: 730px) {
+    font-size: 25px;
+  }
+`;
+
+const Emoji = styled.span`
+  margin-right: 10px;
+  margin-bottom: 5px;
+
+  @media (max-width: 730px) {
+    font-size: 20px;
+  }
+`;
+
+const TitleInfo = styled.p`
+  font-size: 20px;
+  text-align: center;
+
+  @media (max-width: 730px) {
+    font-size: 16px;
+  }
+`;
 
 function Header() {
   const windowWidth = useWindowWidth();
-  
+
   return (
-    <div className="titleDiv">
-      <div className="title">
-        <span id="emoji">🌼</span>
-        <span id="appTitle">해피 저금통</span>
-      </div>
+    <Container>
+      <Title>
+        <Emoji>🌼</Emoji>
+        <span>해피 저금통</span>
+      </Title>
       {windowWidth > 530 ? (
-        <p id="titleInfo">
+        <TitleInfo>
           기쁜 일이나 즐거운 일을 적어 해피 저금통에 넣어주세요.
           <br />
           연말에 개봉해 꺼내 보며 한 해 동안 행복했던 순간들을 떠올려봐요.
-        </p>
+        </TitleInfo>
       ) : (
-        <p id="titleInfo">
+        <TitleInfo>
           기쁜 일이나 즐거운 일을 적어 <br />
           해피 저금통에 넣어주세요.
           <br />
           연말에 개봉해 꺼내 보며 <br />한 해 동안 행복했던 순간들을 떠올려봐요.
-        </p>
+        </TitleInfo>
       )}
-    </div>
+    </Container>
   );
 }
 
